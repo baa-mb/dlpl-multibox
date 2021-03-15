733e9a85c448771a4c5abebc40c6dd4e9cf7a203
baa <34665120+baa-mb@users.noreply.github.com>
2021-03-15 17:13:28 +0100

    Aktualisiere test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 // Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
 //neop_schreibe("C")
-pause(5000)
+pause(2000)
 neop_schreibe_zch("A")
 DLPLneopixel.schreibe_wort(123, MyEnum.One)

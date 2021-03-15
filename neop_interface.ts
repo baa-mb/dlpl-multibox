6e070bb8d80864df3541543aee98a7eeb3ab6824
baa <34665120+baa-mb@users.noreply.github.com>
2021-03-15 16:48:09 +0100

    Aktualisiere pxt.json, neop_interface.ts

---
+++ neop_interface.ts
@@ -0,0 +1,40 @@
+// Gib deinen Code hier ein
+
+/**
+ * Nutze diese Datei für benutzerdefinierte Funktionen und Blöcke.
+ * Weitere Informationen unter https://makecode.microbit.org/blocks/custom
+ */
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Benutzerdefinierte Blöcke
+ */
+//% weight=100 color=#0fbc11 icon="\uf1ec" block="DLPL Neopixel"
+namespace DLPLneopixel {
+    /**
+     * TODO: Beschreibe deine Funktion hier
+     * @param n Beschreibe die Parameter hier, eg: 5
+     * @param s Beschreibe die Parameter hier, eg: "Hello"
+     * @param e Beschreibe die Parameter hier
+     */
+    //% block
+    export function schreibe_zeichen(z: string, e: MyEnum): void {
+        // Add code here
+        neop_schreibe(z)
+    }
+
+    /**
+     * TODO: Beschreibe deine Funktion hier
+     * @param value Beschreibe den Wert hier, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}

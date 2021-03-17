1a46c40446e86902995e34450e00dad177e23d6f
baa <34665120+baa-mb@users.noreply.github.com>
2021-03-17 03:14:57 +0100

    Aktualisiere neop_interface.ts

--- neop_interface.ts
+++ neop_interface.ts
@@ -18,38 +18,27 @@ enum MyPin {
 //% color=190 weight=100 icon="\uf00a" block="DLPL Neopixel"
 //% groups=['Einstellungen', 'Befehle', 'others']
 namespace DLPLneopixel {
-    /**
-     * TODO: Beschreibe deine Funktion hier
-     * @param n Beschreibe die Parameter hier, eg: 5
-     * @param s Beschreibe die Parameter hier, eg: "Hello"
-     * @param e Beschreibe die Parameter hier
-     */
 
     //% group="Grundeinstellungen"
     //% block weight=100
-    export function NeopixelInit(z: string): void {
+    export function neopixelInit(z: string): void {
         // Add code here
         neop_schreibe_zch(z)
     }
     //% group="Hauptbefehle"
-    //% 
+
     //% block blockGap=0
-    export function schreibe_wort(w:string): void {
-        // Add code here
-        neop_schreibe_wort(w)
-    }
     export function schreibe_zeichen(z: string): void {
         // Add code here
         neop_schreibe_zch(z)
     }
 
 
-    /**
-     * TODO: Beschreibe deine Funktion hier
-     * @param value Beschreibe den Wert hier, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    //% block blockGap=0
+    export function schreibe_wort(w:string): void {
+        // Add code here
+        neop_schreibe_wort(w)
     }
+
+
 }

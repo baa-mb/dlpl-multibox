804f2e66bb29184b1fcd99abd9a16a8580b10b64
baa <34665120+baa-mb@users.noreply.github.com>
2021-03-20 17:27:20 +0100

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -260,10 +260,10 @@ interface zch_tab {
   def: Array<number>;
 }
 
-// // hardwareeinstellungen ########################### 3 Matriken
-// let arr_tech_matrix=[[8,8],[5,7],[16,16]];
-// let arr_tech_pin=[DigitalPin.P1,DigitalPin.P2,DigitalPin.P8];
-// // hardwareeinstellungen end ###########################
+// hardwareeinstellungen ########################### 3 Matriken
+let arr_tech_matrix=[[8,8],[5,7],[16,16]];
+let arr_tech_pin=[DigitalPin.P1,DigitalPin.P2,DigitalPin.P8];
+// hardwareeinstellungen end ###########################
 
 
 let bst_muster=[31,31,31,31,31,31,31,31];

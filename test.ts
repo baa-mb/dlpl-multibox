0cf709974af66a5de1bedb641ae63c049b5eeb5a
baa <34665120+baa-mb@users.noreply.github.com>
2021-03-20 03:01:37 +0100

    Aktualisiere test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,3 @@
 // Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
 //neop_schreibe("C")
 
-// neop_schreibe_zch("A")
-
-DLPLneopixel.schreibeText(1, "ABC", 120)
